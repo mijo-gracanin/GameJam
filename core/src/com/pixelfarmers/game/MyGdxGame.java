ae524e28189071ce0ffbf541e9c21e05503d9486
Mijo Gracanin <mijo.gracanin@gmail.com>
2016-01-27 23:34:23 +0100

    Setup starting game environment

---
+++ core/src/com/pixelfarmers/game/MyGdxGame.java
@@ -0,0 +1,13 @@
+package com.pixelfarmers.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxGame extends Game {
+	
+	@Override
+	public void create () {
+		setScreen(new GameScreen());
+	}
+}
---
+++ desktop/src/com/pixelfarmers/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.pixelfarmers.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pixelfarmers.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 640;
+		config.height = 480;
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
