221a3df30707f975ef0a4e2b668e88954695d1cd
Matej Vidakovic <matej.vidakovic@fiveminutes.eu>
2016-01-30 09:09:25 +0100

    added ai library

---
+++ core/src/com/pixelfarmers/goat/Enemy.java
@@ -0,0 +1,8 @@
+package com.pixelfarmers.goat;
+
+import com.badlogic.gdx.ai.steer.SteerableAdapter;
+import com.badlogic.gdx.math.Vector2;
+
+public class Enemy extends SteerableAdapter<Vector2> {
+
+}
