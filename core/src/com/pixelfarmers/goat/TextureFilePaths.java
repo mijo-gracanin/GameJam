57d5a82be93081333b814fea67115f1738839bc3
Matej Vidakovic <matej.vidakovic@fiveminutes.eu>
2016-01-31 16:51:39 +0100

    powerup textures updated

--- core/src/com/pixelfarmers/goat/TextureFilePaths.java
+++ core/src/com/pixelfarmers/goat/TextureFilePaths.java
@@ -5,6 +5,6 @@ public class TextureFilePaths {
     public static final String CHARACTER = "character.png";
     public static final String PROJECTILE = "magic.png";
     public static final String HEALTH_POWERUP = "heart.png";
-    public static final String SPEED_POWERUP = "half_heart.png";
-    public static final String DAMAGE_POWERUP = "candle_1.png";
+    public static final String SPEED_POWERUP = "powerup_milk.png";
+    public static final String DAMAGE_POWERUP = "powerup_damage.png";
 }
